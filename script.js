3e29be26f87022c517b29a826d7e05fa6cd05841
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-04-22 08:53:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+
+headings.forEach(heading => {
+  let letters = heading.textContent.split("");
+  heading.textContent = "";
+
+  letters.forEach(letter => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.className = "fade-in";
+    heading.appendChild(span);
+  });
+});
+
+const spans = document.querySelectorAll('.fade-in');
+
+spans.forEach((span, index) => {
+  span.addEventListener('click', () => {
+    span.classList.add('fade-out');
+    if (index === spans.length - 1) {
+      setTimeout(() => {
+        window.location.href = "https://github.com/Ajay-Dhangar";
+      }, 1000);
+    }
+  });
+});
+
